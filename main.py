ddfa5e95be1a2b279edcee46f7572dc3e8dc8024
bounis67 <boehmcorentin52@gmail.com>
2023-02-16 17:46:33 +0100

    v1.3
    Add ActiveAutoLike

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ minInterval = 2
 maxInterval = 30
 
 # chance to like
-likeChance = 50
+ActiveAutoLike = True
+likeChance = 20
 
 
 # ---------------------------------Requirements----------------------------------------
@@ -35,12 +36,13 @@ while True:
         print(f"Elapsed time: {i+1} seconds out of {compteurtime}")
         time.sleep(1)
         compteurlike = random.randint(0, likeChance)
-        if compteurlike == likeChance-1: # Correction: use 1 instead of 50 to make the condition true
-            nombrelike = nombrelike + 1
-            print(f'Number of likes given: {nombrelike}')
-            keyboard_controller.press("l")
-            time.sleep(0.2)
-            keyboard_controller.release("l")
+        if ActiveAutoLike == True:
+            if compteurlike == likeChance-1: # Correction: use 1 instead of 50 to make the condition true
+                nombrelike = nombrelike + 1
+                print(f'Number of likes given: {nombrelike}')
+                keyboard_controller.press("l")
+                time.sleep(0.2)
+                keyboard_controller.release("l")
             
     keyboard_controller.press(keyboard.Key.down)
     time.sleep(0.2)
